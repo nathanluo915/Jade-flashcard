018f8821e54bd1af59699392a0668daabc9e95d8
Nathan Luo <email": null>
2015-11-13 14:26:18 -0500

    Add migration for models

---
+++ db/migrate/20151113123729_create_rounds.rb
@@ -0,0 +1,11 @@
+class CreateRounds < ActiveRecord::Migration
+  def change
+    create_table :rounds do |t|
+      t.integer :user_id, null: false
+      t.integer :deck_id, null: false
+      t.integer :first_round_correct, null: false, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
